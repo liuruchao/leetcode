be4affcbfc253bf0ec331bb8d08934de93aa77d2
liuruchao <runlrc@163.com>
2020-10-26 23:15:22 +0800

    feat(104二叉树的最大深度): 新增解法： 2.二叉树的层次遍历

--- tree/104二叉树的最大深度.js
+++ tree/104二叉树的最大深度.js
@@ -15,3 +15,39 @@ function maxDepth(root) {
 
   return height
 }
+
+/* 2. 使用层次遍历
+  按照层次遍历，依次遍历节点
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  if (!root) return 0
+
+  let queue = []
+  let depth = 0
+  queue.push(root)
+  while (queue.length) {
+    // 当前层的节点个数
+    let currentLayerLen = queue.length
+    // 遍历该层的节点，边遍历边出队，然后将下一层的节点再入队，遍历完成后，深度加1
+    for (let i = 0; i < currentLayerLen; i++) {
+      let currentNode = queue.shift()
+      currentNode.left && queue.push(currentNode.left)
+      currentNode.right && queue.push(currentNode.right)
+    }
+    depth++
+  }
+
+  return depth
+}
